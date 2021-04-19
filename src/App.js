a23ad14eaba2d11c6dd58c62f73b0645cc527107
CullenSharp2 <cullen.sharp100@gmail.com>
2021-04-19 16:12:44 -0700

    Style map

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Container, Form, Button } from "react-bootstrap";
+import { Container, Form, Button, Card, Navbar } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import axios from "axios";
 
@@ -24,29 +24,45 @@ class App extends React.Component {
 
   render() {
     const img_url = `https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_CITY_KEY}&center=${this.state.location.lat},${this.state.location.lon}&size=${window.innerWidth}x300&format=jpg&zoom=12`;
+
     return (
       <Container flex>
-        <Form>
-          <Form.Group>
-            <Form.Label>enter a city name to explore</Form.Label>
-            <Form.Control
-              type="text"
-              placeholder="City Name"
-              onChange={(e) => this.setState({ searchQuery: e.target.value })}
-            />
-          </Form.Group>
-          <Button variant="primary" type="submit" onClick={this.getLocation}>
-            Explore
-          </Button>
-        </Form>
-        
+        <Navbar
+          bg="light"
+          expand="lg"
+          className="justify-content-md-space-between"
+        >
+          <Navbar.Brand>City Explorer</Navbar.Brand>
+          <Form inline>
+            <Form.Group>
+              <Form.Control
+                type="text"
+                placeholder="City Name"
+                className="mr-sm-2"
+                onChange={(e) => this.setState({ searchQuery: e.target.value })}
+              />
+            </Form.Group>
+            <Button
+              variant="outline-success"
+              type="submit"
+              onClick={this.getLocation}
+            >
+              Explore
+            </Button>
+          </Form>
+        </Navbar>
         {this.state.location.place_id && (
-          <>
-            <img src={img_url} alt="Map" />
-            <h2>The city is: {this.state.location.display_name}</h2>
-            <h2>lat: {this.state.location.lat}</h2>
-            <h2>lon: {this.state.location.lon}</h2>
-          </>
+          <Card style={{ minWidth: "18rem" }}>
+            <Card.Img variant="top" src={img_url} alt="Map" />
+            <Card.Body>
+              <Card.Title>{this.state.location.display_name}</Card.Title>
+              <Card.Text>
+                lat: {this.state.location.lat}
+                <br></br>
+                lon: {this.state.location.lon}
+              </Card.Text>
+            </Card.Body>
+          </Card>
         )}
       </Container>
     );
