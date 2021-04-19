6c7fdae517e34e96376769eafbefab8d3170acf6
CullenSharp2 <cullen.sharp100@gmail.com>
2021-04-19 15:00:58 -0700

    Build out static site

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
-import Container from "react-bootstrap/Container";
+import {Container, Form, Button} from "react-bootstrap";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import axios from "axios";
 
 class App extends React.Component {
@@ -11,10 +12,27 @@ class App extends React.Component {
     };
   }
 
+  getLocation = () => {
+    const API = `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_CITY_KEY}&q=${this.state.searchQuery}&format=json`;
+  }
+
   render() {
     return (
       <Container flex>
-        hello, world
+        <Form>
+          <Form.Group>
+            <Form.Label>enter a city name to explore</Form.Label>
+            <Form.Control 
+            type="text" 
+            placeholder="City Name"
+            onChange={(e) => this.setState({searchQuery:e.target.value})}
+            />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Explore
+          </Button>
+        </Form>
+        <img src="https://www.placecage.com/gif/200/300"></img>
       </Container>
     );
   }
