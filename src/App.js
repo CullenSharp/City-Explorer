3105be4a24d124c1b994411102a07ac04237905b
CullenSharp2 <cullen.sharp100@gmail.com>
2021-04-19 15:26:11 -0700

    Display location name, lat, and lon

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
-import {Container, Form, Button} from "react-bootstrap";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container, Form, Button } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 import axios from "axios";
 
 class App extends React.Component {
@@ -9,12 +9,19 @@ class App extends React.Component {
     this.state = {
       searchQuery: "",
       location: {},
+      url: "",
     };
   }
 
-  getLocation = () => {
+  getLocation = async (e) => {
+    e.preventDefault();
+
     const API = `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_CITY_KEY}&q=${this.state.searchQuery}&format=json`;
-  }
+    const resp = await axios.get(API);
+    console.log(resp.data[0]);
+    // const url = 
+    this.setState({ location: resp.data[0] });
+  };
 
   render() {
     return (
@@ -22,17 +29,24 @@ class App extends React.Component {
         <Form>
           <Form.Group>
             <Form.Label>enter a city name to explore</Form.Label>
-            <Form.Control 
-            type="text" 
-            placeholder="City Name"
-            onChange={(e) => this.setState({searchQuery:e.target.value})}
+            <Form.Control
+              type="text"
+              placeholder="City Name"
+              onChange={(e) => this.setState({ searchQuery: e.target.value })}
             />
           </Form.Group>
-          <Button variant="primary" type="submit">
+          <Button variant="primary" type="submit" onClick={this.getLocation}>
             Explore
           </Button>
         </Form>
-        <img src="https://www.placecage.com/gif/200/300"></img>
+        <img src="https://www.placecage.com/gif/200/300" alt="Placecage" />
+        {this.state.location.place_id && (
+          <>
+            <h2>The city is: {this.state.location.display_name}</h2>
+            <h2>lat: {this.state.location.lat}</h2>
+            <h2>lon: {this.state.location.lon}</h2>
+          </>
+        )}
       </Container>
     );
   }
