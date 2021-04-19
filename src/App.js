7f8f27186112b6a9325d9ab3c07c7496a2282b04
CullenSharp2 <cullen.sharp100@gmail.com>
2021-04-19 14:48:30 -0700

    Start work on form

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Container from "react-bootstrap/Container";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchQuery: "",
+      location: {},
+    };
+  }
+
+  render() {
+    return (
+      <Container flex>
+        hello, world
+      </Container>
+    );
+  }
 }
 
 export default App;
