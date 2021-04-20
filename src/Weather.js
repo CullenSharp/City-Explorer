089184064825213c188eb56a8b4505df1e510f39
CullenSharp2 <cullen.sharp100@gmail.com>
2021-04-20 16:58:57 -0700

    Convert back to class component

--- src/Weather.js
+++ src/Weather.js
@@ -1,14 +1,17 @@
+import React from 'react';
 import ListGroup from 'react-bootstrap/ListGroup'
 import { faCloudSunRain } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-function Weather() {
-    return(
-        <ListGroup.Item>
-            <FontAwesomeIcon icon={faCloudSunRain} className="mr-sm-2"/>
-            {`${this.props.data.date}: ${this.props.data.description}`}
-        </ListGroup.Item>
-    );
+class Weather extends React.Component {
+    render() {
+        return(
+            <ListGroup.Item>
+                <FontAwesomeIcon icon={faCloudSunRain} className="mr-sm-2"/>
+                {`${this.props.data.date}: ${this.props.data.description}`}
+            </ListGroup.Item>
+        );
+    }
 } 
 
 export default Weather;
