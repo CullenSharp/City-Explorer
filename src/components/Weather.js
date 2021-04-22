4f080289007198e82db71c84302c2a9b38a639f3
CullenSharp2 <cullen.sharp100@gmail.com>
2021-04-22 15:40:11 -0700

    Fix linter errors and refactor

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,22 +1,31 @@
 import React from 'react';
-import ListGroup from 'react-bootstrap/ListGroup'
+import ListGroup from 'react-bootstrap/ListGroup';
 import { faCloudSunRain } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 class Weather extends React.Component {
-    render() {
-        return(
-            this.props.weather.map((day, index) =>
-                <ListGroup.Item key={index}>
-                    <FontAwesomeIcon icon={faCloudSunRain} className="mr-sm-2"/>
-                    {`
-                    ${day.date}
-                    ${day.forcast}
-                    `}
-                </ListGroup.Item>
-            )
-        );
-    }
+  render() {
+    return(
+      this.props.weather.map((day, idx) => (
+        <WeatherDay day={day} idx={idx}/>
+      )
+      )
+    );
+  }
+} 
+
+class WeatherDay extends React.Component {
+  render() {
+    return (
+      <ListGroup.Item key={this.props.idx}>
+        <FontAwesomeIcon icon={faCloudSunRain} className="mr-sm-2"/>
+        {`
+        ${this.props.day.date}
+        ${this.props.day.forcast}
+        `}
+      </ListGroup.Item>
+    );
+  }
 } 
 
 export default Weather;
