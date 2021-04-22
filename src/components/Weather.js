2f1d0c733f098d5fb2da463903a597982726c0c0
CullenSharp2 <cullen.sharp100@gmail.com>
2021-04-22 16:21:51 -0700

    Change imports

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import WeatherDay from './WeatherDay.js';
+import WeatherDay from './WeatherDay';
 
 class Weather extends React.Component {
   render() {
     return(
-      this.props.weather.map((day, idx) => (
+      this.props.forcasts.map((day, idx) => (
         <WeatherDay day={day} idx={idx}/>
       )
       )
