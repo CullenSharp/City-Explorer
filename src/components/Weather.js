b26b16e03e60d349ef494c63eb36835fe809fc34
CullenSharp2 <cullen.sharp100@gmail.com>
2021-04-22 15:45:06 -0700

    Import WeatherDay from WeatherDay.js

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,7 +1,5 @@
 import React from 'react';
-import ListGroup from 'react-bootstrap/ListGroup';
-import { faCloudSunRain } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import WeatherDay from './WeatherDay.js';
 
 class Weather extends React.Component {
   render() {
@@ -10,20 +8,6 @@ class Weather extends React.Component {
         <WeatherDay day={day} idx={idx}/>
       )
       )
-    );
-  }
-} 
-
-class WeatherDay extends React.Component {
-  render() {
-    return (
-      <ListGroup.Item key={this.props.idx}>
-        <FontAwesomeIcon icon={faCloudSunRain} className="mr-sm-2"/>
-        {`
-        ${this.props.day.date}
-        ${this.props.day.forcast}
-        `}
-      </ListGroup.Item>
     );
   }
 } 
