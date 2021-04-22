61bbecb0f5bfe8ce71f4a1ef6572fb3f45f23c8a
CullenSharp2 <cullen.sharp100@gmail.com>
2021-04-22 15:44:34 -0700

    Componentize WeatherDay

---
+++ src/components/WeatherDay.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ListGroup from 'react-bootstrap/ListGroup';
+import { faCloudSunRain } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+export default class WeatherDay extends React.Component {
+  render() {
+    return (
+      <ListGroup.Item key={this.props.idx}>
+        <FontAwesomeIcon icon={faCloudSunRain} className="mr-sm-2"/>
+        {`
+        ${this.props.day.date}
+        ${this.props.day.forcast}
+        `}
+      </ListGroup.Item>
+    );
+  }
+} 
