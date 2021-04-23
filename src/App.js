ebdb1d530fa3bced7da2c29d461d9819ac2a07e0
CullenSharp2 <cullen.sharp100@gmail.com>
2021-04-22 17:48:50 -0700

    Fix syntax error

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      searchQuery: ',
+      searchQuery: '',
       location: {},
       forcasts: [],
       movies: [],
